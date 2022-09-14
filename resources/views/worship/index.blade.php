1ea29e296037f939fa2611274a2ea0cd5a6e0f8c
Ariq Daffa <ariqdaffaathallah@gmail.com>
2022-09-14 19:59:40 +0700

    setup worship

--- resources/views/worship/index.blade.php
+++ resources/views/worship/index.blade.php
@@ -1,11 +1,50 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container py-3">
-    <div class="row">
-        <div class="col-12">
-            <h3>Pendaftaran Ibadah</h3>
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-12">
+                <h3>Pendaftaran Ibadah</h3>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6 mt-3">
+                <div class="card">
+                    <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Ibadah Hari Minggu</h5>
+                        <p class="card-text">Tanggal: 19 Juni 2022 <br> Waktu: 19.00 WIB</p>
+                        <div class="d-grid">
+                            <a href="#" class="btn btn-success btn-block stretched-link">Daftar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 mt-3">
+                <div class="card">
+                    <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Ibadah Hari Minggu</h5>
+                        <p class="card-text">Tanggal: 19 Juni 2022 <br> Waktu: 19.00 WIB</p>
+                        <div class="d-grid">
+                            <a href="#" class="btn btn-success btn-block stretched-link">Daftar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 mt-3">
+                <div class="card">
+                    <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Ibadah Hari Minggu</h5>
+                        <p class="card-text">Tanggal: 19 Juni 2022 <br> Waktu: 19.00 WIB</p>
+                        <div class="d-grid">
+                            <a href="#" class="btn btn-success btn-block stretched-link">Daftar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
