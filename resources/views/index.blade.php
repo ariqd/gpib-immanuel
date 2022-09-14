5f64aecf10e98d53802b74e773950d1ba851d9bd
Ariq Daffa <ariqdaffaathallah@gmail.com>
2022-09-14 19:41:06 +0700

    setup home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,55 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container py-3">
-    <div class="row">
-        <div class="col-12">
-            <h3>Home</h3>
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="text-center">Kegiatan Kami</h3>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="#" class="stretched-link text-dark text-decoration-none">Warta Jemaat Minggu, 04 Juli 2022</a>
+                        </h5>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the
+                            card's content.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="#" class="stretched-link text-dark text-decoration-none">Tata Ibadah Minggu, 04 Juli 2022</a>
+                        </h5>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the
+                            card's content.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/300x150" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="#" class="stretched-link text-dark text-decoration-none">Ibadah Hari Minggu, 29 Maret 2022</a>
+                        </h5>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of the
+                            card's content.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
