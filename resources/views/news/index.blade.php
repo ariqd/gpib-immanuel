c24bf745ed972424798304d780bcf72cf6130a6a
Ariq Daffa <ariqdaffaathallah@gmail.com>
2022-09-16 21:02:04 +0700

    prepare news layout

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,11 +1,119 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container py-3">
-    <div class="row">
-        <div class="col-12">
-            <h3>Tata Ibadah & Warta</h3>
+    <div class="container py-3">
+        <div class="row">
+            <div class="col-12">
+                <div class="d-flex justify-content-between">
+                    <h3>Tata Ibadah & Warta</h3>
+                    <div>
+                        <form class="row">
+                            <div class="col-auto">
+                                <label for="date" class="mt-2">Masukkan tanggal</label>
+                            </div>
+                            <div class="col-auto">
+                                <label for="date" class="visually-hidden">Tanggal</label>
+                                <input type="date" class="form-control" id="date" placeholder="Tanggal">
+                            </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-primary">Cari</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <hr>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-6 mb-3">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="https://via.placeholder.com/150x250" alt="...">
+                        </div>
+                        <div class="col-9">
+                            <div class="border-bottom d-flex justify-content-between">
+                                <p class="m-0 p-2">Warta Jemaat</p>
+                                <p class="m-0 p-2">04 Juli 2022</p>
+                            </div>
+                            <p class="mt-2"><b>Hari Minggu Sesudah Pentakosta</b></p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, accusantium! Ratione
+                                voluptatem ducimus doloremque, sunt doloribus nulla, veritatis similique optio nihil
+                                voluptatum, asperiores itaque cum placeat maxime iusto fugiat eveniet.
+                            </p>
+                            <a href="#">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 mb-3">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="https://via.placeholder.com/150x250" alt="...">
+                        </div>
+                        <div class="col-9">
+                            <div class="border-bottom d-flex justify-content-between">
+                                <p class="m-0 p-2">Warta Jemaat</p>
+                                <p class="m-0 p-2">04 Juli 2022</p>
+                            </div>
+                            <p class="mt-2"><b>Hari Minggu Sesudah Pentakosta</b></p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, accusantium! Ratione
+                                voluptatem ducimus doloremque, sunt doloribus nulla, veritatis similique optio nihil
+                                voluptatum, asperiores itaque cum placeat maxime iusto fugiat eveniet.
+                            </p>
+                            <a href="#">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 mb-3">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="https://via.placeholder.com/150x250" alt="...">
+                        </div>
+                        <div class="col-9">
+                            <div class="border-bottom d-flex justify-content-between">
+                                <p class="m-0 p-2">Warta Jemaat</p>
+                                <p class="m-0 p-2">04 Juli 2022</p>
+                            </div>
+                            <p class="mt-2"><b>Hari Minggu Sesudah Pentakosta</b></p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, accusantium! Ratione
+                                voluptatem ducimus doloremque, sunt doloribus nulla, veritatis similique optio nihil
+                                voluptatum, asperiores itaque cum placeat maxime iusto fugiat eveniet.
+                            </p>
+                            <a href="#">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 mb-3">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="https://via.placeholder.com/150x250" alt="...">
+                        </div>
+                        <div class="col-9">
+                            <div class="border-bottom d-flex justify-content-between">
+                                <p class="m-0 p-2">Warta Jemaat</p>
+                                <p class="m-0 p-2">04 Juli 2022</p>
+                            </div>
+                            <p class="mt-2"><b>Hari Minggu Sesudah Pentakosta</b></p>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, accusantium! Ratione
+                                voluptatem ducimus doloremque, sunt doloribus nulla, veritatis similique optio nihil
+                                voluptatum, asperiores itaque cum placeat maxime iusto fugiat eveniet.
+                            </p>
+                            <a href="#">Baca Selengkapnya</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
