4adecac7e05af3c0cecaaf2056ca8136d7e28dd7
Ariq Daffa <ariqdaffaathallah@gmail.com>
2022-09-19 20:12:12 +0700

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="mt-auto py-3" style="background: #34647A; color: white">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <h4>Kontak Kami</h4>
+                <div class="row mt-3">
+                    <div class="col-12 d-flex">
+                        <i class="bi bi-envelope-fill"></i>
+                        <p class="mx-3">gpibimmanuelpekanbaru@gmail.com</p>
+                    </div>
+                    <div class="col-12 d-flex">
+                        <i class="bi bi-telephone-fill"></i>
+                        <p class="mx-3">(0888) 8888888</p>
+                    </div>
+                    <div class="col-12 d-flex">
+                        <i class="bi bi-clock-fill"></i>
+                        <p class="mx-3">Selasa - Sabtu: 08:30 - 16:00</p>
+                    </div>
+                    <div class="col-12 d-flex">
+                        <i class="bi bi-geo-alt-fill"></i>
+                        <p class="mx-3">Jl. Sumatera no. 21-23, Simpang Empat, Kec. Pekanbaru Kota, Kota Pekanbaru, Riau</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <h4>Map GPIB Immanuel Pekanbaru</h4>
+                <div class="row mt-3">
+                    <div class="col-12 d-flex">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,15 +7,17 @@
     <title>GPIB Immanuel</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 
 <body>
     @include('components.navbar')
 
-    @yield('content')
+    <body class="d-flex flex-column min-vh-100">
+        @yield('content')
+    </body>
+
+    @include('components.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
