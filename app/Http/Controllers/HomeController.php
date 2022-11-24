dcfa74ab0a708cd15b8e2f5f867a5056b89f8f6e
Ariq Daffa <ariqdaffaathallah@gmail.com>
2022-11-24 11:31:21 +0700

    remove comments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\News;
 use App\Models\Worship;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        // dd(Worship::whereDate('worship_date', '>', Carbon::now())->first());
         return view('index', [
             'warta' => News::where('news_type', 'Warta')->latest()->first(),
             'tata' => News::where('news_type', 'Tata Ibadah')->latest()->first(),
