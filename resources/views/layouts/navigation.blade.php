a457e8ee4e79e5d5e17a55aeed77546206017a01
Ariq Daffa <ariqdaffaathallah@gmail.com>
2023-03-09 11:37:47 +0700

    chore: remove comments

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,16 +6,12 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('admin.worships.index') }}">
-                        {{-- <x-application-logo class="block h-10 w-auto fill-current text-gray-600" /> --}}
                         GPIB Immanuel
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    {{-- <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link> --}}
                     <x-nav-link :href="route('admin.worships.index')" :active="request()->routeIs('admin.worships*')">
                         {{ __('Ibadah') }}
                     </x-nav-link>
@@ -87,9 +83,6 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            {{-- <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link> --}}
             <x-responsive-nav-link :href="route('admin.worships.index')" :active="request()->routeIs('admin.worships*')">
                 {{ __('Ibadah') }}
             </x-responsive-nav-link>
