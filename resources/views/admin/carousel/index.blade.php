7bcfa1cdf6b2ccec8f3a5c2a45dc6665161a13a0
Ariq Daffa <ariqdaffaathallah@gmail.com>
2023-04-13 12:21:22 +0700

    change wrong title

--- resources/views/admin/carousel/index.blade.php
+++ resources/views/admin/carousel/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex justify-between items-baseline">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Tata Ibadah & Warta') }}
+                {{ __('Carousel') }}
             </h2>
             <a type="button" href="{{ route('admin.carousel.create') }}"
                 class="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 focus:outline-none">
